580612f9842605b79254b5d495f654e25bdf2506
Zelourses <24527982+Zelourses@users.noreply.github.com>
2022-06-29 17:32:41 +0300

    Fixed assertion macro (suddenly, I forgot to finish it, so asserts actually were empty all that time)

--- Real/src/real/core/debug/Assert.h
+++ Real/src/real/core/debug/Assert.h
@@ -9,13 +9,13 @@
 	#define RE_INTERNAL_ASSERT_IMPL(type, check, msg, ...) \
 		{                                                  \
 			if (!(check)) {                                \
-				RE##typeCRITICAL(msg, __VA_ARGS__);        \
-				RE_DEBUGBRAK();                            \
+				RE##type##CRITICAL(msg, __VA_ARGS__);        \
+				RE_DEBUGBREAK();                           \
 			}                                              \
 		}
 
-	#define RE_INTERNAL_ASSERT_WITH_MSG(type, check, ...)
-	#define RE_INTERNAL_ASSERT_NO_MSG(type, check)
+	#define RE_INTERNAL_ASSERT_WITH_MSG(type, check, ...) RE_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}'", __VA_ARGS__)
+	#define RE_INTERNAL_ASSERT_NO_MSG(type, check)		  RE_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", RE_STRINGIFY(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 	#define RE_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define RE_INTERNAL_ASSERT_GET_MACRO(...)						  RE_EXPAND_MACRO(RE_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, RE_INTERNAL_ASSERT_WITH_MSG, RE_INTERNAL_ASSERT_NO_MSG))
